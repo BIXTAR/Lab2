9d3d82eab4d389c79792779fb5b33aa3b5a774f2
BIXTAR <daniil3614@gmail.com>
2025-06-19 08:05:45 +0700

    2. Добавление checkFactorialBtn

--- app/src/main/java/com/labforce/lab2/MainActivity.kt
+++ app/src/main/java/com/labforce/lab2/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.labforce.lab2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val numberInput = findViewById<EditText>(R.id.numberInput)
+        val checkFactorialBtn = findViewById<Button>(R.id.checkFactorialBtn)
+
+        checkFactorialBtn.setOnClickListener {
+            val numberText = numberInput.text.toString()
+            val n = numberText.toIntOrNull()
+
+
+            val fact = factorial(n)
+
+            var found = false
+            for (i in 1..fact.toInt()) {
+                if (BigInteger.valueOf(i.toLong())
+                        .multiply(BigInteger.valueOf(i + 1L))
+                        .multiply(BigInteger.valueOf(i + 2L)) == fact
+                ) {
+                    found = true
+                    break
+                }
+            }
+
         }
     }
-}
